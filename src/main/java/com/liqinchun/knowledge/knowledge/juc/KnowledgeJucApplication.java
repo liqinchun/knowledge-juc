8441e7d3e270ec2556a43db894296f9919fd69fd
liqinchun <liqc@inhand.com.cn>
2021-03-16 11:26:14 +0800

    first commit

---
+++ src/main/java/com/liqinchun/knowledge/knowledge/juc/KnowledgeJucApplication.java
@@ -0,0 +1,13 @@
+package com.liqinchun.knowledge.knowledge.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeJucApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liqinchun/knowledge/knowledge/juc/KnowledgeJucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liqinchun.knowledge.knowledge.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeJucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
